b3add248a3ca3ad981870c4d99175e8223d3963f
lachlankitchen <lachlan.kitchen123@gmail.com>
2023-08-10 11:42:17 +1200

    fix

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:testapp/main.dart';
+import 'package:edu_app/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
