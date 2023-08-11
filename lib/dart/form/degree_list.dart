7d39f761c89be132f8f2d508ca1e00f8359da33d
lachlankitchen <lachlan.kitchen123@gmail.com>
2023-08-11 12:03:46 +1200

    Add list of button to navigate to major list

--- lib/dart/form/degree_list.dart
+++ lib/dart/form/degree_list.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import '../degree/degree.dart';
+import '../degree/degree.dart'; 
+import '../major/major.dart';
+import '../major/major_list.dart'; 
 
 class DegreesListScreen extends StatelessWidget {
   final List<Degree> degrees;
@@ -10,16 +14,86 @@ class DegreesListScreen extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: const Text('Degrees List'),
+        title: const Text('Undergraduate Degrees'),
       ),
       body: ListView.builder(
         itemCount: degrees.length,
         itemBuilder: (context, index) {
           return ListTile(
-            title: Text('${degrees[index].title} in ${degrees[index].field}'),
-            subtitle: Text('Year: ${degrees[index].year}'),
+            
+            title: ElevatedButton(
+              onPressed: () {
+                navigateToMajorsListScreen(context);
+              },
+              child: Text(degrees[index].toString()),
+            ),
           );
         },
+      ),
+    );
+  }
+
+
+  void navigateToMajorsListScreen(BuildContext context) {
+
+    const String majorsJson = '''
+    [
+      {
+        "name": "English",
+        "requirements": [
+          {
+            "level": 100,
+            "papers": ["ENGL 121", "ENGL 131"],
+            "selectOneFrom": ["ENGL 120", "ENGL 121", "ENGL 127", "ENGL 128", "ENGL 131", "LING 111"]
+          },
+          {
+            "level": 200,
+            "papers": ["Three 200-level ENGL papers", "DHUM 201 or EURO 202"]
+          },
+          {
+            "level": 300,
+            "papers": ["Four 300-level ENGL papers", "EURO 302"]
+          },
+          {
+            "points": 198,
+            "notes": "Must include 54 points at 200-level or above. Up to 90 points may be taken from outside Arts."
+          }
+        ],
+        "totalPoints": 360
+      },
+      {
+        "name": "Computer Science",
+        "requirements": [
+          {
+            "level": 100,
+            "papers": ["CS 101", "CS 102"],
+            "selectOneFrom": ["MATH 120", "MATH 121"]
+          },
+          {
+            "level": 200,
+            "papers": ["CS 201", "CS 202"]
+          },
+          {
+            "level": 300,
+            "papers": ["CS 301", "CS 302"]
+          },
+          {
+            "points": 180,
+            "notes": "Must include at least 60 points at 300-level or above."
+          }
+        ],
+        "totalPoints": 360
+      }
+    ]
+    ''';
+
+    List<dynamic> parsedMajorsJson = json.decode(majorsJson);
+    List<Major> majors = parsedMajorsJson.map((majorJson) => Major.fromJson(majorJson)).toList();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MajorListScreen(majors: majors),
       ),
     );
   }
