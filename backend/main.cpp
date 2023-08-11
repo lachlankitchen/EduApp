ada0e8b3dff06b1341a58c47f777a6681e69881b
choskin1 <connorhoskin@yahoo.co.nz>
2023-08-11 15:31:52 +1200

    main backend

--- backend/main.cpp
+++ backend/main.cpp
@@ -1,6 +1,23 @@
-#include <iostream>
+#define CROW_MAIN // Define this before including Crow header.
+#include "crow_all.h"
 
-int main() {
-    std::cout << "Hello, world!" << std::endl;
-    return 0;
+int main()
+{
+    crow::SimpleApp app;
+
+    // A simple endpoint that returns a greeting message.
+    CROW_ROUTE(app, "/greeting")
+    ([]()
+     { return "Hello from the C++ backend!"; });
+
+    // An endpoint that returns a simple JSON object.
+    CROW_ROUTE(app, "/data")
+    ([]()
+     {
+        crow::json::wvalue x;
+        x["message"] = "Here's some data from the backend.";
+        x["number"] = 42;
+        return x; });
+
+    app.port(4000).run(); // Run the server on port 4000.
 }
