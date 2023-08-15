6dcf60b487ffca5804df14e8db1c75b6bfaf3dae
lachlankitchen <lachlan.kitchen123@gmail.com>
2023-08-15 20:39:06 +1200

    move checkbox to the right

--- lib/dart/paper/paper_list.dart
+++ lib/dart/paper/paper_list.dart
@@ -3,31 +3,32 @@ import 'package:provider/provider.dart';
 import '../home/home.dart';
 import '../paper/paper.dart';
 import '../pathway/pathway_state.dart';
+
+
 class PapersListScreen extends StatelessWidget {
   final List<Paper> papers;
 
   PapersListScreen(this.papers);
 
   @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Papers List'),
-      ),
-      body: StatefulBuilder(
-        builder: (context, setState) {
-          return ListView.builder(
-            itemCount: papers.length,
-            itemBuilder: (context, index) {
-              return ListTile(
-                title: Text('${papers[index].subjectCode} - ${papers[index].title}'),
-                subtitle: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    const SizedBox(height: 10),
-                    Row(
+Widget build(BuildContext context) {
+  return Scaffold(
+    appBar: AppBar(
+      title: const Text('Papers List'),
+    ),
+    body: StatefulBuilder(
+      builder: (context, setState) {
+        return ListView.builder(
+          itemCount: papers.length,
+          itemBuilder: (context, index) {
+            return ListTile(
+              title: Row(
+                children: [
+                  Expanded(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        const Text('Enter Grade: '),
+                        Text('${papers[index].subjectCode} - ${papers[index].title}'),
                         SizedBox(
                           width: 80,
                           child: TextField(
@@ -46,51 +47,52 @@ class PapersListScreen extends StatelessWidget {
                             },
                           ),
                         ),
-                        Checkbox(
-                          value: papers[index].isSelected,
-                          onChanged: (bool? value) {
-                            setState(() {
-                              papers[index].isSelected = value ?? false;
-                            });
-                          },
-                        ),
                       ],
                     ),
-                  ],
-                ),
-              );
-            },
-          );
-        },
-      ),
-      floatingActionButton: ElevatedButton(
-        onPressed: () {
-          final state = Provider.of<PathwayState>(context, listen: false);
-          List<Paper> selectedPapers = papers.where((paper) => paper.isSelected).toList();
-          
-          // Calculate GPA based on selected papers' grades
-          double totalWeightedSum = 0;
-          int totalWeight = 0;
+                  ),
+                  Checkbox(
+                    value: papers[index].isSelected,
+                    onChanged: (bool? value) {
+                      setState(() {
+                        papers[index].isSelected = value ?? false;
+                      });
+                    },
+                  ),
+                ],
+              ),
+            );
+          },
+        );
+      },
+    ),
+    floatingActionButton: ElevatedButton(
+      onPressed: () {
+        final state = Provider.of<PathwayState>(context, listen: false);
+        List<Paper> selectedPapers = papers.where((paper) => paper.isSelected).toList();
 
-          for (int i = 0; i < selectedPapers.length; i++) {
-            totalWeightedSum += selectedPapers[i].grade * selectedPapers[i].points;
-            totalWeight += selectedPapers[i].points;
-          }
+        // Calculate GPA based on selected papers' grades
+        double totalWeightedSum = 0;
+        int totalWeight = 0;
 
-          double wam = totalWeightedSum / totalWeight;
-          double gpa = (wam / 10) * (9/10);
-          state.addGPA(gpa);
-          print('GPA: ${state.gpa}');
-          state.addPapers(selectedPapers);
-          state.saveState();
-          Navigator.pushReplacement(
-            context,
-            MaterialPageRoute(builder: (context) => const MyHomePage()),
-          );
-        },
-        child: const Text('Save'),
-      ),
-    );
-  }
+        for (int i = 0; i < selectedPapers.length; i++) {
+          totalWeightedSum += selectedPapers[i].grade * selectedPapers[i].points;
+          totalWeight += selectedPapers[i].points;
+        }
+
+        double wam = totalWeightedSum / totalWeight;
+        double gpa = (wam / 10) * (9/10);
+        state.addGPA(gpa);
+        state.addPapers(selectedPapers);
+        state.saveState();
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(builder: (context) => const MyHomePage()),
+        );
+      },
+      child: const Text('Save'),
+    ),
+  );
+}
+
 }
 
