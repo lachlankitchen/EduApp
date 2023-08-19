9bd1a25aa9c04da520f1be3e5e70653e535aa8f6
lachlankitchen <lachlan.kitchen123@gmail.com>
2023-08-19 12:43:37 +1200

    fix

--- lib/dart/degree/degree_list.dart
+++ lib/dart/degree/degree_list.dart
@@ -1,5 +1,9 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import '../major/major.dart';
+import '../major/major_list.dart';
 import 'degree.dart';
 import '../pathway/pathway_state.dart';
 
@@ -10,49 +14,109 @@ class DegreeListScreen extends StatelessWidget {
   const DegreeListScreen({Key? key, required this.degrees, required this.onSelectDegree})
       : super(key: key);
 
-  @override
+ @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Select a Degree'),
         backgroundColor: const Color(0xFF10428C),
       ),
-      body: Padding(
-        padding: const EdgeInsets.symmetric(horizontal: 50.0),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          children: [
-            const SizedBox(height: 25.0), // Add space from header to first degree list item
-            ...degrees.map((degree) {
-              return Padding(
-                padding: const EdgeInsets.only(top: 8.0, bottom: 8.0),
+      body: ListView.builder(
+        itemCount: degrees.length,
+        itemBuilder: (context, index) {
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0), // Adjust vertical padding
+            child: ListTile(
+              title: Hero(
+                tag: 'degree-${degrees[index].title}',
                 child: ElevatedButton(
                   onPressed: () {
-                    onSelectDegree(degree);
-                    navigateToMajorsListScreen(context, context.read<PathwayState>(), degree);
+                    onSelectDegree(degrees[index]);
+                    navigateToMajorsListScreen(context, context.read<PathwayState>(), degrees[index]);
                   },
                   style: ElevatedButton.styleFrom(
-                    backgroundColor: const Color(0xFFf9c000),
-                    padding: const EdgeInsets.all(12.0),
+                    backgroundColor: const Color(0xFFf9c000), // Set background color here
                     shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(8.0),
+                      borderRadius: BorderRadius.circular(8.0), // Add rounded corners
                     ),
+                    padding: const EdgeInsets.all(14.0), // Adjust padding
                   ),
                   child: Text(
-                    degree.title,
-                    style: const TextStyle(fontSize: 16.0),
+                    degrees[index].title,
+                    style: const TextStyle(fontSize: 16.0), // Increase font size
                   ),
                 ),
-              );
-            }).toList(),
-          ],
-        ),
+              ),
+            ),
+          );
+        },
       ),
     );
   }
 
-  void navigateToMajorsListScreen(
-      BuildContext context, PathwayState state, Degree selectedDegree) {
-    // Implement the rest of your navigation logic here
+  void navigateToMajorsListScreen(BuildContext context, PathwayState state, Degree selectedDegree) {
+    // Pass the selected degree to the state
+    state.addDegree(selectedDegree);
+
+    const String majorsJson = '''
+    [
+      {
+        "name": "English",
+        "requirements": [
+          {
+            "level": 100,
+            "papers": ["ENGL 121", "ENGL 131"],
+            "selectOneFrom": ["ENGL 120", "ENGL 121", "ENGL 127", "ENGL 128", "ENGL 131", "LING 111"]
+          },
+          {
+            "level": 200,
+            "papers": ["Three 200-level ENGL papers", "DHUM 201 or EURO 202"]
+          },
+          {
+            "level": 300,
+            "papers": ["Four 300-level ENGL papers", "EURO 302"]
+          },
+          {
+            "points": 198,
+            "notes": "Must include 54 points at 200-level or above. Up to 90 points may be taken from outside Arts."
+          }
+        ],
+        "totalPoints": 360
+      },
+      {
+        "name": "Computer Science",
+        "requirements": [
+          {
+            "level": 100,
+            "papers": ["CS 101", "CS 102"],
+            "selectOneFrom": ["MATH 120", "MATH 121"]
+          },
+          {
+            "level": 200,
+            "papers": ["CS 201", "CS 202"]
+          },
+          {
+            "level": 300,
+            "papers": ["CS 301", "CS 302"]
+          },
+          {
+            "points": 180,
+            "notes": "Must include at least 60 points at 300-level or above."
+          }
+        ],
+        "totalPoints": 360
+      }
+    ]
+    ''';
+
+    List<dynamic> parsedMajorsJson = json.decode(majorsJson);
+    List<Major> majors = parsedMajorsJson.map((majorJson) => Major.fromJson(majorJson)).toList();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MajorListScreen(majors: majors),
+      ),
+    );
   }
 }
