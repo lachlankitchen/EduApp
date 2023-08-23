20d3a4576d4e4b5c77631c5a8c4f69fcc11a6da3
lachlankitchen <lachlan.kitchen123@gmail.com>
2023-08-23 19:09:58 +1200

    Test Codedoc.xyz w/ c++ style comments

--- lib/dart/degree/degree_list.dart
+++ lib/dart/degree/degree_list.dart
@@ -1,65 +1,142 @@
+import 'dart:convert';
+
 import 'package:flutter/material.dart';
-import 'package:pie_chart/pie_chart.dart'; /* Import the pie chart package */
 import 'package:provider/provider.dart';
-import '../navigation/nav_bar.dart';
-import '../pathway/pathway.dart';
+import '../major/major.dart';
+import '../major/major_list.dart';
+import 'degree.dart';
 import '../pathway/pathway_state.dart';
 
-/* A screen that displays the distribution of points across selected majors using a pie chart. */
-class DegreesPointsScreen extends StatelessWidget {
-  /* Constructs a DegreesPointsScreen instance. */
-  const DegreesPointsScreen({super.key});
+/// A screen that displays a list of degrees to choose from.
+class DegreeListScreen extends StatelessWidget {
+  final List<Degree> degrees;
+  final Function(Degree) onSelectDegree;
+
+  /// Constructs a [DegreeListScreen].
+  ///
+  /// [degrees] is the list of available degrees to display.
+  /// [onSelectDegree] is a callback function when a degree is selected.
+  const DegreeListScreen({
+    Key? key,
+    required this.degrees,
+    required this.onSelectDegree,
+  }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
-    final state = Provider.of<PathwayState>(context, listen: false);
-
-    // Calculate the total points from all majors
-    final dataMap = <String, double>{};
-  
-    double totalPoints = 0;
-    if (state.savedPathways.isNotEmpty) {
-      Pathway pathway = state.savedPathways[0];
-      for (var major in pathway.majors) {
-        totalPoints = 0;
-        for (var paper in pathway.papers) {
-          totalPoints += paper.points;
-        }
-        dataMap[major.name] = totalPoints;
-      }
-    }    
-
-    // Calculate the remaining points
-    dataMap["Remaining Points"] = (360 - totalPoints);
-    
     return Scaffold(
-      bottomNavigationBar: const NavBar(),
       appBar: AppBar(
-        title: const Text('Points'),
+        title: const Text('Select a Degree'),
         backgroundColor: const Color(0xFF10428C),
       ),
-      body: Column(
-        children: [
-          Expanded( // Wrap the PieChart with an Expanded widget
-            child: Container(
-              padding: const EdgeInsets.all(16),
-              child: PieChart(
-                dataMap: dataMap,
-                chartType: ChartType.ring,
-                chartRadius: MediaQuery.of(context).size.width / 2.5,
-                ringStrokeWidth: 32,
-                chartValuesOptions: const ChartValuesOptions(
-                  showChartValuesOutside: true,
-                  showChartValuesInPercentage: false,
-                ),
-                legendOptions: const LegendOptions(
-                  legendPosition: LegendPosition.bottom,
-                  showLegendsInRow: true,
+      body: ListView.builder(
+        itemCount: degrees.length + 1, // Add 1 for SizedBox
+        itemBuilder: (context, index) {
+          if (index == 0) {
+            return const SizedBox(height: 18.0); // Add padding at the top
+          }
+
+          final degreeIndex = index - 1; // Subtract 1 to adjust for SizedBox
+          return Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 10.0),
+            child: ListTile(
+              title: Hero(
+                tag: 'degree-${degrees[degreeIndex].title}',
+                child: ElevatedButton(
+                  onPressed: () {
+                    onSelectDegree(degrees[degreeIndex]);
+                    navigateToMajorsListScreen(
+                        context, context.read<PathwayState>(), degrees[degreeIndex]);
+                  },
+                  style: ElevatedButton.styleFrom(
+                    backgroundColor: const Color(0xFFf9c000),
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(8.0),
+                    ),
+                    padding: const EdgeInsets.all(14.0),
+                  ),
+                  child: Text(
+                    degrees[degreeIndex].title,
+                    style: const TextStyle(fontSize: 16.0),
+                  ),
                 ),
               ),
             ),
-          ),
+          );
+        },
+      ),
+    );
+  }
+
+  /// Navigates to the major list screen.
+  ///
+  /// [context]: The build context to perform navigation.
+  /// [state]: The state containing pathway information.
+  /// [selectedDegree]: The selected degree to add to the pathway state.
+  ///
+  /// This function does not return a value.
+  void navigateToMajorsListScreen(BuildContext context, PathwayState state, Degree selectedDegree) {
+    // Pass the selected degree to the state
+    state.addDegree(selectedDegree);
+
+    const String majorsJson = '''
+    [
+      {
+        "name": "English",
+        "requirements": [
+          {
+            "level": 100,
+            "papers": ["ENGL 121", "ENGL 131"],
+            "selectOneFrom": ["ENGL 120", "ENGL 121", "ENGL 127", "ENGL 128", "ENGL 131", "LING 111"]
+          },
+          {
+            "level": 200,
+            "papers": ["Three 200-level ENGL papers", "DHUM 201 or EURO 202"]
+          },
+          {
+            "level": 300,
+            "papers": ["Four 300-level ENGL papers", "EURO 302"]
+          },
+          {
+            "points": 198,
+            "notes": "Must include 54 points at 200-level or above. Up to 90 points may be taken from outside Arts."
+          }
         ],
+        "totalPoints": 360
+      },
+      {
+        "name": "Computer Science",
+        "requirements": [
+          {
+            "level": 100,
+            "papers": ["CS 101", "CS 102"],
+            "selectOneFrom": ["MATH 120", "MATH 121"]
+          },
+          {
+            "level": 200,
+            "papers": ["CS 201", "CS 202"]
+          },
+          {
+            "level": 300,
+            "papers": ["CS 301", "CS 302"]
+          },
+          {
+            "points": 180,
+            "notes": "Must include at least 60 points at 300-level or above."
+          }
+        ],
+        "totalPoints": 360
+      }
+    ]
+    ''';
+
+    List<dynamic> parsedMajorsJson = json.decode(majorsJson);
+    List<Major> majors = parsedMajorsJson.map((majorJson) => Major.fromJson(majorJson)).toList();
+
+    Navigator.push(
+      context,
+      MaterialPageRoute(
+        builder: (context) => MajorListScreen(majors: majors),
       ),
     );
   }
