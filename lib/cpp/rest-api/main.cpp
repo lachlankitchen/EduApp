4f6e0cba94cf049be119165cc1993fcf987ebd3c
lachlankitchen <lachlan.kitchen123@gmail.com>
2023-09-14 13:50:34 +1200

    Modify API to send JSON responses

--- lib/cpp/rest-api/main.cpp
+++ lib/cpp/rest-api/main.cpp
@@ -40,3 +40,52 @@ int main(void)
 
   svr.listen("localhost", 1234);
 }
+
+// #include "httplib.h"
+// #include <nlohmann/json.hpp> // Include the JSON library
+
+// using json = nlohmann::json; // Alias for JSON
+
+// int main(void) {
+//     using namespace httplib;
+
+//     Server svr;
+
+//     svr.Get("/hi", [](const Request& req, Response& res) {
+//         json response_json = {{"message", "Hello World!"}}; // Create a JSON object
+//         res.set_content(response_json.dump(), "application/json"); // Set content type to JSON
+//     });
+
+//     // Match the request path against a regular expression
+//     // and extract its captures
+//     svr.Get(R"(/numbers/(\d+))", [&](const Request& req, Response& res) {
+//         auto numbers = req.matches[1];
+//         json response_json = {{"number", numbers}}; // Create a JSON object
+//         res.set_content(response_json.dump(), "application/json"); // Set content type to JSON
+//     });
+
+//     // Capture the second segment of the request path as "id" path param
+//     svr.Get("/users/:id", [&](const Request& req, Response& res) {
+//         auto user_id = req.path_params.at("id");
+//         json response_json = {{"user_id", user_id}}; // Create a JSON object
+//         res.set_content(response_json.dump(), "application/json"); // Set content type to JSON
+//     });
+
+//     // Extract values from HTTP headers and URL query params
+//     svr.Get("/body-header-param", [](const Request& req, Response& res) {
+//         if (req.has_header("Content-Length")) {
+//             auto val = req.get_header_value("Content-Length");
+//         }
+//         if (req.has_param("key")) {
+//             auto val = req.get_param_value("key");
+//         }
+//         json response_json = {{"body", req.body}}; // Create a JSON object
+//         res.set_content(response_json.dump(), "application/json"); // Set content type to JSON
+//     });
+
+//     svr.Get("/stop", [&](const Request& req, Response& res) {
+//         svr.stop();
+//     });
+
+//     svr.listen("localhost", 1234);
+// }
