2e96f9a7a5004e728d684df6751e0c6ce5cd8d22
lachlankitchen <lachlan.kitchen123@gmail.com>
2023-09-16 16:53:47 +1200

    Add required endpoints to serve front-end

--- lib/cpp/rest-api/main.cpp
+++ lib/cpp/rest-api/main.cpp
@@ -3,43 +3,74 @@
 #include <filesystem>
 #include <fstream>
 
+void cors(const Request& req, Response& res) {
+    res.set_header("Access-Control-Allow-Origin", "*");
+    res.set_header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+}
+
 int main(void)
 {
   using namespace httplib;
 
   Server svr;
 
-  svr.Get("/degrees", [](const Request &req, Response &res)
-          {
-       std::filesystem::path json_file_path = "../../../data/degrees.json"; // Specify the correct path to your JSON file
-       if (!std::filesystem::exists(json_file_path)) {
-           res.status = 404;
-           res.set_content("File not found", "text/plain");
-           std::cout << "File not found at specified path.\n";
-           return;
-       }
+  svr.Get("/degrees", [](const Request &req, Response &res){
+      std::filesystem::path json_file_path = "../../../data/degrees.json"; // Specify the correct path to your JSON file
+      if (!std::filesystem::exists(json_file_path)) {
+          res.status = 404;
+          res.set_content("File not found", "text/plain");
+          std::cout << "File not found at specified path.\n";
+          return;
+      }
 
-       std::ifstream json_file(json_file_path);
-       std::string json_content((std::istreambuf_iterator<char>(json_file)),
-                                std::istreambuf_iterator<char>());
+      std::ifstream json_file(json_file_path);
+      std::string json_content((std::istreambuf_iterator<char>(json_file)),
+                              std::istreambuf_iterator<char>());
 
-       res.set_header("Access-Control-Allow-Origin", "*");
-       res.set_header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
-       res.set_content(json_content, "application/json");
+      cors(req, res);
 
-       std::cout << "Successfully served the JSON file.\n"; 
+      res.set_content(json_content, "application/json");
+
+      std::cout << "Successfully served the JSON file.\n"; 
   });
 
   svr.Get("/major/{degrees}", [](const Request &req, Response &res){
-  
-  }
+      cors(req, res);
+
+      res.set_content(json_content, "application/json");
+
+      std::cout << "Successfully served the JSON file.\n"; 
+  });
 
   svr.Get("/paper/{paperCode}", [](const Request &req, Response &res){
-  
-  }
+      cors(req, res);
+
+      res.set_content(json_content, "application/json");
+
+      std::cout << "Successfully served the JSON file.\n"; 
+  });
 
 
   svr.Get("/userPaperList", [](const Request &req, Response &res){
-  
-  }
+      cors(req, res);
+
+      res.set_content(json_content, "application/json");
+
+      std::cout << "Successfully served the JSON file.\n"; 
+  });
+
+  // Extract values from HTTP headers and URL query params
+  svr.Get("/body-header-param", [](const Request& req, Response& res) {
+    if (req.has_header("Content-Length")) {
+      auto val = req.get_header_value("Content-Length");
+    }
+    if (req.has_param("key")) {
+      auto val = req.get_param_value("key");
+    }
+    res.set_content(req.body, "text/plain");
+  });
+
+  svr.Get("/stop", [&](const Request& req, Response& res) {
+    svr.stop();
+  });
 }
