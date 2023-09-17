2c1e8ac193e15ccb6cdf7b4e6961a9ea471fcfb1
choskin1 <134260042+choskin1@users.noreply.github.com>
2023-09-18 08:10:46 +1200

    added header files

--- lib/cpp/paper_checker.cpp
+++ lib/cpp/paper_checker.cpp
@@ -1,61 +1,52 @@
+#include "paper_checker.h"
 #include <iostream>
-#include <vector>
-#include <string>
 #include <unordered_set>
-#include <nlohmann/json.hpp> // Include the nlohmann/json library for JSON parsing
 
-using json = nlohmann::json;
 using namespace std;
 
-class DegreeRequirements {
-private:
-    json degreeData;
+DegreeRequirements::DegreeRequirements(const string& jsonData) {
+    degreeData = json::parse(jsonData);
+}
 
-public:
-    DegreeRequirements(const string& jsonData) {
-        degreeData = json::parse(jsonData);
-    }
+bool DegreeRequirements::checkRequirements(const vector<string>& completedPapers) {
+    unordered_set<string> completedSet(completedPapers.begin(), completedPapers.end());
 
-    bool checkRequirements(const vector<string>& completedPapers) {
-        unordered_set<string> completedSet(completedPapers.begin(), completedPapers.end());
-
-        for (const auto& level : degreeData["levels"]) {
-            // Check compulsory papers
-            if (level.contains("compulsory_papers")) {
-                for (const auto& paper : level["compulsory_papers"]) {
-                    if (completedSet.find(paper) == completedSet.end()) {
-                        cout << "Missing compulsory paper: " << paper << endl;
-                        return false;
-                    }
-                }
-            }
-
-            // Check one_of_papers
-            if (level.contains("one_of_papers")) {
-                bool oneOfPassed = false;
-                for (const auto& paper : level["one_of_papers"]) {
-                    if (completedSet.find(paper) != completedSet.end()) {
-                        oneOfPassed = true;
-                        break;
-                    }
-                }
-                if (!oneOfPassed) {
-                    cout << "Must complete at least one of the papers in the list for " << level["level"] << endl;
+    for (const auto& level : degreeData["levels"]) {
+        // Check compulsory papers
+        if (level.contains("compulsory_papers")) {
+            for (const auto& paper : level["compulsory_papers"]) {
+                if (completedSet.find(paper) == completedSet.end()) {
+                    cout << "Missing compulsory paper: " << paper << endl;
                     return false;
                 }
             }
         }
 
-        // Check total points (assuming each paper is worth a certain number of points)
-        int totalPoints = completedPapers.size() * 18; // Assuming each paper is 18 points
-        if (totalPoints < degreeData["total"]) {
-            cout << "Total points not met. Required: " << degreeData["total"] << ", Achieved: " << totalPoints << endl;
-            return false;
+        // Check one_of_papers
+        if (level.contains("one_of_papers")) {
+            bool oneOfPassed = false;
+            for (const auto& paper : level["one_of_papers"]) {
+                if (completedSet.find(paper) != completedSet.end()) {
+                    oneOfPassed = true;
+                    break;
+                }
+            }
+            if (!oneOfPassed) {
+                cout << "Must complete at least one of the papers in the list for " << level["level"] << endl;
+                return false;
+            }
         }
-
-        return true;
     }
-};
+
+    // Check total points (assuming each paper is worth a certain number of points)
+    int totalPoints = completedPapers.size() * 18; // Assuming each paper is 18 points
+    if (totalPoints < degreeData["total"]) {
+        cout << "Total points not met. Required: " << degreeData["total"] << ", Achieved: " << totalPoints << endl;
+        return false;
+    }
+
+    return true;
+}
 
 int main() {
     string jsonData = R"({
