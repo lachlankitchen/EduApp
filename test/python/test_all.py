eccecd8a2d6a0f55b896354ff076277675131bd9
choskin1 <134260042+choskin1@users.noreply.github.com>
2023-09-18 11:45:01 +1200

    added tests for python classes

---
+++ test/python/test_all.py
@@ -0,0 +1,31 @@
+# test/python/test_all.py
+
+from lib.python.allDegree import *  # replace with actual class or function names
+from lib.python.degreeURL import *  # replace with actual class or function names
+from lib.python.paperscraper import *  # replace with actual class or function names
+from lib.python.qualifications import *  # replace with actual class or function names
+from lib.python.schedule_scraper import *  # replace with actual class or function names
+from lib.python.test import *  # replace with actual class or function names
+from lib.python.testServer import *  # replace with actual class or function names
+
+# Here create your test functions, one for each function or method you want to test
+def test_allDegree():
+    assert True
+
+def test_degreeURL():
+    assert True
+
+def test_paperscraper():
+    assert True
+
+def test_qualifications():
+    assert True
+
+def test_schedule_scraper():
+    assert True
+
+def test_test():
+    assert True
+
+def test_testServer():
+    assert True
