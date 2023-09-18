5f0f1d8ded72296d6c75ca8fc51b425ad0b676d3
lachlankitchen <lachlan.kitchen123@gmail.com>
2023-09-18 12:06:59 +1200

    Add endpoint to validate paper selection

--- lib/cpp/rest-api/main.cpp
+++ lib/cpp/rest-api/main.cpp
@@ -53,11 +53,37 @@ int main(void)
 
     std::cout << "Successfully served the JSON file.\n"; });
 
-  svr.Get("/degree/majors/:major", [&](const Request &req, Response &res)
-          {
-    auto degree = req.path_params.at("major"); // TODO: @CONNOR Utilise when quering by major
+  svr.Get("/:degree/:major", [&](const Request &req, Response &res)
+            {
+    auto major = req.path_params.at("major"); // TODO: @CONNOR Utilise when quering by major
 
     std::filesystem::path json_file_path = std::filesystem::path("..") / ".." / ".." / "data" / "major_requirements.json";
+
+    if (!std::filesystem::exists(json_file_path)) {
+        res.status = 404;
+        res.set_content("File not found", "text/plain");
+        std::cout << "File not found at specified path.\n";
+        return;
+    }
+
+    std::ifstream json_file(json_file_path);
+    std::string json_content((std::istreambuf_iterator<char>(json_file)),
+                            std::istreambuf_iterator<char>());
+
+    res.set_header("Access-Control-Allow-Origin", "*");
+    res.set_header("Access-Control-Allow-Headers", "X-Requested-With, Content-Type");
+    res.set_content(json_content, "application/json");
+
+    std::cout << "Successfully served the JSON file.\n"; });
+
+  svr.Post("/:degree/:major", [&](const Request &req, Response &res)
+            {
+    std::cout << "POST request received.\n";
+
+    auto degree = req.path_params.at("degree"); // TODO: @CONNOR Utilise when quering by major
+    auto major = req.path_params.at("major"); // TODO: @CONNOR Utilise when quering by major
+
+    std::filesystem::path json_file_path = std::filesystem::path("..") / ".." / ".." / "data" / "paper_response.json";
 
     if (!std::filesystem::exists(json_file_path)) {
         res.status = 404;
