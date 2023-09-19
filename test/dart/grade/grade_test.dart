3b0cb9b1157fbac800cb51c4fe3ebd6ea520dde6
lachlankitchen <lachlan.kitchen123@gmail.com>
2023-09-19 12:34:45 +1200

    Test behaviour of grades and pathway classes

--- test/dart/grade/grade_test.dart
+++ test/dart/grade/grade_test.dart
@@ -1,7 +1,6 @@
 import 'package:edu_app/dart/grade/grade.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'grades.dart'; // Import your GradesScreen widget here
 
 void main() {
   testWidgets('GradesScreen Widget Test', (WidgetTester tester) async {
@@ -14,19 +13,35 @@ void main() {
     expect(find.text('Average: 0.0'), findsOneWidget);
 
     // Enter values in the text fields.
-    await tester.enterText(find.byLabelText('Title'), 'Assignment 2');
-    await tester.enterText(find.byLabelText('Score (0-100)'), '85');
-    await tester.enterText(find.byLabelText('Weight (%)'), '20');
+    await tester.enterText(find.byKey(const Key('TitleField')), 'Assignment 1');
+    await tester.enterText(find.byKey(const Key('ScoreField')), '85');
+    await tester.enterText(find.byKey(const Key('WeightField')), '20');
 
     // Tap the Add button.
     await tester.tap(find.byType(FloatingActionButton));
     await tester.pump();
 
     // Verify that the average has been updated.
-    expect(find.text('Average: 17.0'), findsOneWidget);
+    expect(find.text('Average: 85.0'), findsOneWidget);
 
     // Verify that the added grade entry is displayed in the list view.
-    expect(find.text('Assignment 2'), findsOneWidget);
+    expect(find.text('Assignment 1'), findsOneWidget);
     expect(find.text('Score: 85.00, Weight: 20.00%'), findsOneWidget);
+
+        // Enter values in the text fields.
+    await tester.enterText(find.byKey(const Key('TitleField')), 'Final Exam');
+    await tester.enterText(find.byKey(const Key('ScoreField')), '60');
+    await tester.enterText(find.byKey(const Key('WeightField')), '50');
+
+    // Tap the Add button.
+    await tester.tap(find.byType(FloatingActionButton));
+    await tester.pump();
+
+    // Verify that the average has been updated.
+    expect(find.text('Average: 67.1'), findsOneWidget);
+
+    // Verify that the added grade entry is displayed in the list view.
+    expect(find.text('Final Exam'), findsOneWidget);
+    expect(find.text('Score: 60.00, Weight: 50.00%'), findsOneWidget);
   });
 }
--- test/dart/home/pathway_widget_test.dart
+++ test/dart/home/pathway_widget_test.dart
@@ -1,23 +1,51 @@
-import 'dart:convert';
-
 import 'package:edu_app/dart/degree/degree.dart';
 import 'package:edu_app/dart/home/home.dart';
 import 'package:edu_app/dart/major/major.dart';
 import 'package:edu_app/dart/navigation/navigation_provider.dart';
+import 'package:edu_app/dart/paper/paper.dart';
+import 'package:edu_app/dart/pathway/display_pathway.dart';
+import 'package:edu_app/dart/pathway/pathway.dart';
 import 'package:edu_app/dart/pathway/pathway_state.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
-import 'package:http/http.dart' as http; // Import http package
 import 'package:provider/provider.dart';
+import 'package:http/http.dart' as http; // Import http package
 import '../utils/mock_client.dart';
 
 void main() {
+  // Create mock data for degree, major, compulsory papers, and one-of papers
+  final Degree mockDegree1 = Degree('Bachelor of Science');
+  final Degree mockDegree2 = Degree('Bachelor of Arts');
+
+  final List<Major> mockMajors = [
+    Major.withName(name: 'Computer Science'),
+    Major.withName(name: 'Oceanography'),
+    Major.withName(name: 'Zoology')
+  ];
+
+  final List<Paper> mockPapers = [
+    Paper.withName(
+      papercode: 'CS 101',
+      title: 'Introduction to Computer Science',
+      teachingPeriods: ['S1', 'S2'],
+    ),
+    Paper.withName(
+      papercode: 'CS 161',
+      title: 'Computer Programming',
+      teachingPeriods: ['SS', 'S2'],
+    ),
+    Paper.withName(
+      papercode: 'CS 200',
+      title: 'Advanced Computer Science',
+      teachingPeriods: ['SS', 'S2'],
+    )
+  ];
+
   testWidgets('Test MyHomePage Widget', (WidgetTester tester) async {
-    final Degree mockDegree = Degree('Bachelor of Science');
 
     // Create a MockClient with the mockDegree and null for majors
-    final mockClient = MockClient(degree: mockDegree);
-    
+    final mockClient = MockClient(degree: mockDegree1);
+
     // Assign the mock client to the global http client
     http.Client client = mockClient;
 
@@ -46,5 +74,82 @@ void main() {
 
     // Verify that the snackbar is dismissed
     expect(find.text('You cannot have more than three degrees.'), findsNothing);
+  });
+
+  test('PathwayState Test', () {
+    final pathwayState = PathwayState();
+
+    pathwayState.addDegree(mockDegree1);
+    pathwayState.addMajors(List.of([mockMajors[0], mockMajors[1]]));
+    pathwayState.addPapers(mockPapers);
+    pathwayState.addGPA(3.5);
+
+    expect(pathwayState.selectedDegree, mockDegree1);
+    expect(pathwayState.selectedMajors, List.of([mockMajors[0], mockMajors[1]]));
+    expect(pathwayState.selectedPapers, mockPapers);
+    expect(pathwayState.gpa, 3.5);
+
+    pathwayState.saveState();
+
+    final savedPathways = pathwayState.savedPathways;
+
+    expect(savedPathways.length, 1);
+    expect(savedPathways[0].degree, mockDegree1);
+    expect(savedPathways[0].majors, List.of([mockMajors[0], mockMajors[1]]));
+    expect(savedPathways[0].papers, mockPapers);
+    expect(savedPathways[0].gpa, 3.5);
+    expect(savedPathways[0].isSelected, false);
+
+    pathwayState.deleteState(savedPathways[0]);
+
+    expect(pathwayState.savedPathways.length, 0);
+  });
+
+  testWidgets('Test DisplayPathway Widget', (WidgetTester tester) async {
+
+    // Create a list of mock Pathway objects
+    final mockPathways = [
+      Pathway(
+        degree: mockDegree1,
+        majors: List.of([mockMajors[0]]),
+        papers: mockPapers,
+        gpa: 3.5,
+        isSelected: false,
+      ),
+      Pathway(
+        degree: mockDegree2,
+        majors: List.of([mockMajors[1], mockMajors[2]]),
+        papers: mockPapers,
+        gpa: 7.8,
+        isSelected: false,
+      ),
+    ];
+
+    // Build our app and trigger a frame.
+    await tester.pumpWidget(
+      MaterialApp(
+        home: DisplayPathway(
+          pathway: mockPathways,
+        ),
+      ),
+    );
+
+    // Verify that the degree titles are displayed correctly
+    expect(find.text('Bachelor of Science'), findsOneWidget);
+    expect(find.text('Bachelor of Arts'), findsOneWidget);
+
+    // Verify that the major names are displayed correctly
+    expect(find.text('  Computer Science, '), findsOneWidget);
+    expect(find.text('  Oceanography, '), findsOneWidget);
+    expect(find.text('  Zoology, '), findsOneWidget);
+
+
+    // Verify that the paper names are displayed correctly
+    expect(find.text('  CS 101 - Introduction to Computer Science, '), findsNWidgets(2));
+
+    // TODO: Fix GPA text test
+    // Verify that the GPA is displayed correctly 
+    // expect(find.text('  3.5'), findsOneWidget);
+    // expect(find.text('  7.8'), findsOneWidget);
   });
 }
