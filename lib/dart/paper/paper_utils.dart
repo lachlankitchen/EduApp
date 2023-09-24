2de0daead78440f0db4f14da677070525d82e4da
lachlankitchen <lachlan.kitchen123@gmail.com>
2023-09-25 10:08:32 +1300

    Remove duplicate/unused methods

--- lib/dart/paper/paper_utils.dart
+++ lib/dart/paper/paper_utils.dart
@@ -6,48 +6,6 @@ import 'package:http/http.dart' as http;
 import '../degree/degree.dart';
 import '../major/major.dart';
 
-  List<Paper> getLevelPapers(Map<String, dynamic> parsedData, String levelKey, String paperKey) {
-    List<Paper> papers = [];
-    for (var level in parsedData['levels']) {
-      if (level['level'] == levelKey) {
-        var paperList = level[paperKey] as List<dynamic>?; // Use null-safe List
-        if (paperList != null) {
-          for (var paperData in paperList) {
-            for (var entry in paperData.entries) {
-              final paperCode = entry.key;
-              final attributes = entry.value as Map<String, dynamic>;
-              final teachingPeriods = (attributes['teaching_periods'] as List<dynamic>)
-                ?.map<String>((period) => period.toString())
-                ?.toList() ?? []; // Provide a default value if needed
-              final points = attributes['points'];
-
-              papers.add(Paper.withName(
-                papercode: paperCode,
-                title: attributes['title'] ?? '', // Provide a default value if needed
-                teachingPeriods: teachingPeriods, // Provide a default value if needed
-                points: points
-              ));
-            }
-          }
-        }
-      }
-    }
-    return papers;
-  }
-
-  Future<List<String>> fetchAllPapers(String degree, int level) async {
-    final response = await http.get(Uri.parse('http://localhost:1234/$degree/papers/$level'));
-
-    if (response.statusCode == 200) {
-      // If the server did return an OK response, parse the JSON
-      Map<String, dynamic> data = jsonDecode(response.body);
-      return List<String>.from(data['papers']);
-    } else {
-      // If the server did not return a 200 OK response, throw an exception.
-      throw Exception('Failed to load $degree papers');
-    }
-  }
-
   Future<String> fetchRecommendedPapers(Degree degree, Major major, int level) async {
     final response = await http.get(Uri.parse('http://localhost:1234/${degree.title}/${major.name}/papers/$level'));
 
@@ -77,4 +35,27 @@ import '../major/major.dart';
       return Paper.fromJson(papercode, paperData);
     }).toList();
     return recommendedPapers;
+  }
+
+  Future<String> postPaperData(Degree degree, Major major, List<Paper> papersList) async {
+    final url = Uri.parse('http://localhost:1234/${degree.title}/${major.name}');
+   
+    List<Map<String, dynamic>> jsonPapers = papersListToJson(papersList); 
+    String papersJsonString = jsonEncode(jsonPapers);
+    
+    final response = await http.post(
+      url,
+      // @CONNOR Using the JSON string as the request body doen't work, I'm working on a fix
+      // body: papersJsonString, // Set the JSON string as the request body
+      // headers: {
+      //   'Content-Type': 'application/json', // Set the Content-Type header
+      // }
+    );
+    
+    if (response.statusCode == 200) {
+      return response.body;
+    } else {
+      // If the server did not return a 200 OK response, throw an exception.
+      throw Exception('Failed to validate pathway');
+    }
   }
