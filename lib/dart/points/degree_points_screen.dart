ed5cb0bed95a0d4e7e3a54d197fa18e2e35f19ff
lachlankitchen <lachlan.kitchen123@gmail.com>
2023-10-11 23:42:48 +1300

    Display message if no saved pathways

--- lib/dart/points/degree_points_screen.dart
+++ lib/dart/points/degree_points_screen.dart
@@ -22,54 +22,56 @@ class DegreesPointsScreen extends StatelessWidget {
       body: Column(
         children: [
           Expanded(
-            child: ListView.builder(
-              itemCount: state.savedPathways.length,
-              itemBuilder: (context, index) {
-                if(state.savedPathways.isEmpty) {
-                  return const Center(
-                    child: Text("No saved degrees"),
-                  );
-                }
-                final pathway = state.savedPathways[index];
-                final dataMap = <String, double>{};
-                double totalPoints = 0;
+            child: state.savedPathways.isEmpty
+                ? const Center(child: Text("No saved degrees"))
+                : ListView.builder(
+                    itemCount: state.savedPathways.length,
+                    itemBuilder: (context, index) {
+                      if (state.savedPathways.isEmpty) {
+                        return const Center(
+                          child: Text("No saved degrees"),
+                        );
+                      }
+                      final pathway = state.savedPathways[index];
+                      final dataMap = <String, double>{};
+                      double totalPoints = 0;
 
-                // Calculate total points for the current pathway
-                for (var major in pathway.majors) {
-                  List<Paper> allPapers = [];
-                  pathway.selectedPapers.forEach((level, papers) {
-                    allPapers.addAll(papers);
-                  });
+                      // Calculate total points for the current pathway
+                      for (var major in pathway.majors) {
+                        List<Paper> allPapers = [];
+                        pathway.selectedPapers.forEach((level, papers) {
+                          allPapers.addAll(papers);
+                        });
 
-                  for (var paper in allPapers) {
-                    totalPoints += paper.points;
-                  }
-                  dataMap[major.name] = totalPoints;
-                }
+                        for (var paper in allPapers) {
+                          totalPoints += paper.points;
+                        }
+                        dataMap[major.name] = totalPoints;
+                      }
 
-                // Calculate remaining points for the current pathway
-                dataMap["Remaining Points"] = (360 - totalPoints);
+                      // Calculate remaining points for the current pathway
+                      dataMap["Remaining Points"] = (360 - totalPoints);
 
-                // Return a PieChart for the current pathway
-                return Container(
-                  padding: const EdgeInsets.only(top: 48, bottom: 16),
-                  child: PieChart(
-                    dataMap: dataMap,
-                    chartType: ChartType.ring,
-                    chartRadius: MediaQuery.of(context).size.width / 2.5,
-                    ringStrokeWidth: 32,
-                    chartValuesOptions: const ChartValuesOptions(
-                      showChartValuesOutside: true,
-                      showChartValuesInPercentage: false,
-                    ),
-                    legendOptions: const LegendOptions(
-                      legendPosition: LegendPosition.bottom,
-                      showLegendsInRow: true,
-                    ),
+                      // Return a PieChart for the current pathway
+                      return Container(
+                        padding: const EdgeInsets.only(top: 48, bottom: 16),
+                        child: PieChart(
+                          dataMap: dataMap,
+                          chartType: ChartType.ring,
+                          chartRadius: MediaQuery.of(context).size.width / 2.5,
+                          ringStrokeWidth: 32,
+                          chartValuesOptions: const ChartValuesOptions(
+                            showChartValuesOutside: true,
+                            showChartValuesInPercentage: false,
+                          ),
+                          legendOptions: const LegendOptions(
+                            legendPosition: LegendPosition.bottom,
+                            showLegendsInRow: true,
+                          ),
+                        ),
+                      );
+                    },
                   ),
-                );
-              },
-            ),
           ),
         ],
       ),
